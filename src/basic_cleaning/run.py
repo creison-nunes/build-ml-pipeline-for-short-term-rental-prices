c8e100b2c2cbbbb85a4fc84bb9a1e4dfbbf4d56e
creisonnunes <creison.maique@gmail.com>
2026-04-30 08:52:39 -0300

    Pipeline without correction

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -42,10 +42,6 @@ def go(args):
     )
     df = df[df["price"].between(args.min_price, args.max_price)]
 
-    # Filter rows outside NYC geographic boundaries
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
-
     # Convert last_review to datetime
     df["last_review"] = pd.to_datetime(df["last_review"])
 
